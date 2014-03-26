417715c9d79cec6ab3eae5c44e889ac2ce66afd8
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-26 14:23:20 -0500

    added option to automatically order query results

--- src/Stillat/Database/Model.php
+++ src/Stillat/Database/Model.php
@@ -11,17 +11,57 @@ abstract class Model extends Ardent {
 	 * 
 	 * @var array
 	 */
-	protected $order_by = array(null, null);
+	protected $orderBy = array(null, null);
 
+	/**
+	 * Determines if the model will automatically order query results.
+	 * 
+	 * @var boolean
+	 */
+	protected $automaticallyOrder = false;
+
+	/**
+	 * A list of method names that are ignored when determining if the model
+	 * should automatically ordery the query.
+	 * 
+	 * @var array
+	 */
+	protected $ignoredOrderMethods = array('increment', 'decrement');
+
+	/**
+	 * Orders a given query.
+	 * 
+	 * @param  \Illuminate\Database\Eloquent\Builder $query
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
 	public function scopeOrder($query)
 	{
-		if ($this->order_by[0] !=== null and $this->order_by[1] !=== null)
+		if ($this->orderBy[0] !=== null and $this->orderBy[1] !=== null)
 		{
-			return $query->orderBy($this->order_by[0], $this->order_by[1]);
+			return $query->orderBy($this->orderBy[0], $this->orderBy[1]);
 		}
 
-		// If the order_by values are invalid, let's just return the query for now.
+		// If the orderBy values are invalid, let's just return the query for now.
 		return $query;
+	}
+
+	/**
+	 * Handle dynamic method calls into the method.
+	 *
+	 * @param  string  $method
+	 * @param  array   $parameters
+	 * @return mixed
+	 */
+	public function __call($method, $parameters)
+	{
+		// Handle the automatic ordering of query results.
+		if ($this->automaticallyOrder and !in_array($method, $this->ignoredOrderMethods))
+		{
+			return $this->scopeOrder($this->newQuery());
+		}
+
+		// Handle the default Eloquent model __call
+		return parent::__call($method, $parameters);
 	}
 
 	/**
