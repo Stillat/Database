af9163fd6626d578029de733d4d1306d39dfc4c2
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-26 13:37:02 -0500

    Added a "Sort" scope
    this allows users to define an order by column and direction and then
    can use things such as User::where('votes', '>', 100)->sort();

--- src/Stillat/Database/Model.php
+++ src/Stillat/Database/Model.php
@@ -5,6 +5,26 @@ use LaravelBook\Ardent\Ardent;
 abstract class Model extends Ardent {
 
 	/**
+	 * The order by column name and order. The array is setup such
+	 * that the first element is the column name and the second column
+	 * is the sorting direction (i.e., ASC or DESC).
+	 * 
+	 * @var array
+	 */
+	protected $order_by = array(null, null);
+
+	public function scopeSort($query)
+	{
+		if ($this->order_by[0] !=== null and $this->order_by[1] !=== null)
+		{
+			return $query->orderBy($this->order_by[0], $this->order_by[1]);
+		}
+
+		// If the order_by values are invalid, let's just return the query for now.
+		return $query;
+	}
+
+	/**
 	 * Returns the next record.
 	 *
 	 * @return Stillat\Common\Database\Model
