99c4d99348c1d6517b023f220c1ce5eb25901fc8
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-26 21:20:54 -0500

    Ported artisan commands
    moved the artisan commands from the old code base. still need to update
    namespaces and make sure all features work with the new classes (some of
    them have changed)

---
+++ src/Stillat/Database/Tenant/Migrations/TenantMigrationResolver.php
@@ -0,0 +1,20 @@
+<?php namespace Stillat\Database\Tenant\Migrations;
+
+class TenantMigrationResolver {
+
+	/**
+	 * Resolve a migration class name from a file.
+	 *
+	 * @param  string  $file
+	 * @return string
+	 */
+	public function resolveMigrationName($file)
+	{
+		$file = implode('_', array_slice(explode('_', $file), 4));
+
+		$class = studly_case($file);
+
+		return $class;
+	}
+
+}
