671e9d2d362337468b5b4d5f385e285d8a22220d
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-26 21:42:40 -0500

    service provider registers schema manager

--- src/Stillat/Database/DatabaseServiceProvider.php
+++ src/Stillat/Database/DatabaseServiceProvider.php
@@ -1,6 +1,21 @@
 <?php namespace Stillat\Database;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
+use Stillat\Database\Tenant\TenantManager;
+use Illuminate\Database\Migrations\Migrator;
+use Stillat\Database\Tenant\Migrations\TenantMigrator;
+use Stillat\Database\SchemaCreator\SchemaCreatorManager;
+use Stillat\Database\Tenant\Repositories\DatabaseTenantRepository;
+use Stillat\Database\Console\Tenant\NameCommand as TenantNameCommand;
+use Stillat\Database\Console\Tenant\DropCommand as TenantDropCommand;
+use Stillat\Database\Console\Tenant\ResetCommand as TenantResetCommand;
+use Stillat\Database\Console\Tenant\CreateCommand as TenantCreateCommand;
+use Stillat\Database\Console\Tenant\MigrateCommand as TenantMigrateCommand;
+use Stillat\Database\Console\Tenant\InstallCommand as TenantInstallCommand;
+use Stillat\Database\Console\Tenant\RollbackCommand as TenantRollbackCommand;
+use Stillat\Database\Console\Tenant\UninstallCommand as TenantUninstallCommand;
+use Stillat\Database\Console\Tenant\MigrationsCommand as TenantMigrationsCommand;
 
 class DatabaseServiceProvider extends ServiceProvider {
 
@@ -28,7 +43,15 @@ class DatabaseServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		//
+		$this->registerSchemaManager();
+	}
+
+	public function registerSchemaManager()
+	{
+		$this->app['stillat.database.tenant.schema'] = $this->app->share(function($app)
+		{
+			return new SchemaCreatorManager($this->app, $this->app['migration.repository']);
+		});
 	}
 
 	/**
