ed432d146158e5a8864f1d4d49bcba0f72e9aea4
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-26 19:30:29 -0500

    added configuration directives.

---
+++ src/Stillat/Database/Tenant/TenantManager.php
@@ -0,0 +1,37 @@
+<?php namespace Stillat\Database\Tenant;
+
+use Illuminate\Foundation\Application;
+use Stillat\Common\Exceptions\InvalidArgumentException;
+use Stillat\Database\SchemaCreator\SchemaCreatorManager;
+
+class TenantManager {
+
+	/**
+	 * The name of the tenancy session directive.
+	 *
+	 * @var string
+	 */
+	const TENANT_SESSION_DIRECTIVE_NAME = 'tenancy.connectionName';
+
+	/**
+	 * The separator to use for separating schema names from prefixes.
+	 *
+	 * @var string
+	 */
+	const TENANT_SCHEMA_SEPARATOR = '_';
+
+	/**
+	 * The name of the Laravel configuration directive for database connections.
+	 *
+	 * @var string
+	 */
+	const CONFIGURATION_KEY_NAME_PREFIX = 'database.connections';
+
+	/**
+	 * The name of the Laravel configuration directive for default database connections.
+	 *
+	 * @var string
+	 */
+	const CONFIGURATION_DEFAULT_CONNECTION_NAME = 'database.default';
+
+}
